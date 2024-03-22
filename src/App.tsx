e902f0223f6b644cf299fbb0660b05e214b677a7
Gokul <gokulchandan24@gmail.com>
2024-03-22 17:05:34 +0530

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import {
+  Authenticated,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { authProvider, dataProvider, liveProvider } from "./providers";
 import { ForgotPassword, Home, Login, Register } from "./pages";
+import { Layout } from "./components/layout";
 
 const API_BASE_URL = "https://api.crm.refine.dev";
 const API_URL = `{ API_BASE_URL } / graphql`;
@@ -50,6 +57,18 @@ function App() {
                 />
                 <Route path="login" index element={<Login />} />
                 <Route path="register" index element={<Register />} />
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                ></Route>
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
