d9122d41eb9a088d33a1d79f750c48b0b4686cbe
Gokul <gokulchandan24@gmail.com>
2024-03-23 15:01:12 +0530

    comps added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,32 @@
+import UpcomingEvents from "../../components/home/upcoming-events";
+import DealsChart from "../../components/home/deals-chart";
+import { Col, Row } from "antd";
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-
+    <div>
+      <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          <DealsChart />
+        </Col>
+      </Row>
+    </div>
+  );
+};
